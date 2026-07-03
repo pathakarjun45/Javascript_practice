1d8d803c5677f8169a9d9811fb23b48ddaaa8cc2
ArjunPathak002 <arjunpathakdev@gmail.com>
2026-07-03 15:23:36 +0530

    Comaprision Operaters

---
+++ comparision.js
@@ -0,0 +1,29 @@
+console.log(5 > 8);
+console.log(4 >= 6);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log("Pathak");
+console.log(5 != 5);
+console.log(5 !== 5);
+console.log("Arjun");
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == undefined);
+console.log(null >= undefined);
+
+console.log("more comparision");
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+console.log(2 == "2");
+console.log(2 === "2");
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(true == 1);
+console.log(true === 1);
+
+console.log(false == null);
+console.log(false == undefined);
