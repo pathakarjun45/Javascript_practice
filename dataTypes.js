95d1585bd93dd2f81da53b06c3b50b46c8409e3c
ArjunPathak002 <arjunpathakdev@gmail.com>
2026-07-03 17:10:12 +0530

    Heap&Stack

--- dataTypes.js
+++ dataTypes.js
@@ -1,5 +1,8 @@
 // In js have many data types, but the most common ones are:
 // Number, String, Boolean, symbol, null, undefined, bigint, object, array, function
+// Data types are divided into two categories:
+// 1. Primitive data types: Number, String, Boolean, symbol, null, undefined, bigint
+// 2. Non-primitive data types: object, array, function
 
 let numbers = 45;
 let Srings = "Rohit Sharma";
@@ -49,3 +52,40 @@ console.log(typeof nursingId);
 console.log(typeof Bf);
 console.log(typeof amanchutiya);
 console.log(typeof bigints);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                     Stack Memory & Heap Memory
+
+// Stack Memory: It is used to store primitive data types (number, string, boolean, null, undefined, symbol, bigint)
+// they are only copied by value, if we change the value of one variable, it will not change the value of other variable.
+
+// Heap Memory: It is used to store non-primitive data types (object, array, function)
+// they are share the same memory location in heap memory, if we change the value of one variable, it will change the value of other variable also.
+
+let name1 = "Sumant";
+let name2 = name1;
+
+name2 = "Arjun Pathak";
+
+console.log(name1);
+console.log(name2);
+
+const user1 = {
+  name: "Sumant",
+};
+
+const user2 = user1;
+
+user2.name = "Arjun";
+
+console.log(user1.name);
+console.log(user2.name);
+
+const user3 = {
+  name: "Atul",
+  age: 30,
+};
+const user4 = user3;
+user4.age = 31;
+console.log(user3);
+console.log(user4);
